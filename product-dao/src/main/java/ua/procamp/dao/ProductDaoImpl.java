1a8c1fef5e0e8fb0bad0b9299c1b2e3bd39065d6
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-03 22:14:23 +0300

    Implement findAll method
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- product-dao/src/main/java/ua/procamp/dao/ProductDaoImpl.java
+++ product-dao/src/main/java/ua/procamp/dao/ProductDaoImpl.java
@@ -10,17 +10,18 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static java.util.Objects.*;
+import static java.util.Objects.requireNonNull;
 
 public class ProductDaoImpl implements ProductDao {
 
-    private final String FIND_ALL_QUERY = "SELECT p.* FROM products p WHERE p.id = ?";
-    private final String SQL_FIND_ALL_PRODUCTS = "SELECT * FROM products;";
     private final String INSERT_QUERY = "INSERT INTO products (name, producer, price, expiration_date) VALUES (?, ?, ?, ?);";
+    private final String FIND_ALL_QUERY = "SELECT * FROM products;";
+    private final String FIND_QUERY = "SELECT p.* FROM products p WHERE p.id = ?";
     private final String SQL_UPDATE_PRODUCT =
             "UPDATE products SET (name, producer, price, expiration_date, creation_time) = (?, ?, ?, ?, ?)" +
-            "WHERE products.id = ?";
+                    "WHERE products.id = ?";
     private final String SQL_REMOVE_PRODUCT =
             "DELETE FROM products WHERE products.id = ?";
 
@@ -33,7 +34,7 @@ public class ProductDaoImpl implements ProductDao {
     @Override
     public void save(Product product) {
         requireNonNull(product);
-        try(Connection connection = dataSource.getConnection()) {
+        try (Connection connection = dataSource.getConnection()) {
             PreparedStatement insertStatement = prepareInsertStatement(product, connection);
             insertStatement.executeUpdate();
             Long id = fetchGeneratedId(insertStatement);
@@ -67,9 +68,37 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public List<Product> findAll() {
+        try (Connection connection = dataSource.getConnection()) {
+            return fetchAllProducts(connection);
+        } catch (SQLException e) {
+            throw new DaoOperationException("Problem while findAll operation", e);
+        }
+    }
+
+    private List<Product> fetchAllProducts(Connection connection) throws SQLException {
         List<Product> products = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement(FIND_ALL_QUERY);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            products.add(getProductFromRow(resultSet));
+        }
         return products;
     }
+
+    private Product getProductFromRow(ResultSet resultSet) {
+        try {
+            long id = resultSet.getLong("id");
+            String name = resultSet.getString("name");
+            String producer = resultSet.getString("producer");
+            BigDecimal price = resultSet.getBigDecimal("price");
+            LocalDate localDate = resultSet.getDate("expiration_date").toLocalDate();
+            LocalDateTime localDateTime = resultSet.getTimestamp("creation_time").toLocalDateTime();
+            return new Product(id, name, producer, price, localDate, localDateTime);
+        } catch (SQLException e) {
+            throw new DaoOperationException("Cannot parse product from result set", e);
+        }
+    }
+
 
     @Override
     public Product findOne(Long id) {
