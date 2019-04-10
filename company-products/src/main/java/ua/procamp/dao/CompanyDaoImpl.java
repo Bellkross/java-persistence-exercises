9e5b6c7be371dd0643e279deff0e134bbd392bb5
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 00:05:15 +0300

    All tests are completed in company-products
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- company-products/src/main/java/ua/procamp/dao/CompanyDaoImpl.java
+++ company-products/src/main/java/ua/procamp/dao/CompanyDaoImpl.java
@@ -1,8 +1,11 @@
 package ua.procamp.dao;
 
+import ua.procamp.exception.CompanyDaoException;
 import ua.procamp.model.Company;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.Objects;
 
 public class CompanyDaoImpl implements CompanyDao {
     private EntityManagerFactory entityManagerFactory;
@@ -13,6 +16,20 @@ public class CompanyDaoImpl implements CompanyDao {
 
     @Override
     public Company findByIdFetchProducts(Long id) {
-        throw new UnsupportedOperationException("I'm still not implemented!");
+        Objects.requireNonNull(id);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Company c;
+        try {
+            entityManager.getTransaction().begin();
+            c = entityManager.createQuery(
+                    "select c from company c left join fetch c.products where c.id = :id",
+                    Company.class
+            ).setParameter("id", id).getSingleResult();
+        } catch (Exception e) {
+            throw new CompanyDaoException(String.format("No entity found with id %d", id), e);
+        } finally {
+            entityManager.close();
+        }
+        return c;
     }
 }
--- company-products/src/main/java/ua/procamp/model/Company.java
+++ company-products/src/main/java/ua/procamp/model/Company.java
@@ -2,39 +2,33 @@ package ua.procamp.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * todo:
- * - implement no arguments constructor
- * - implement getters and setters for all fields
- * - implement equals() and hashCode() based on identifier field
- * - make setter for field {@link Company#products} private
- * - initialize field {@link Company#products} as new {@link ArrayList}
- * - implement a helper {@link Company#addProduct(Product)} that establishes a relation on both sides
- * - implement a helper {@link Company#removeProduct(Product)} that drops a relation on both sides
- * <p>
- * - configure JPA entity
- * - specify table name: "company"
- * - configure auto generated identifier
- * - configure mandatory column "name" for field {@link Company#name}
- * <p>
- * - configure one to many relationship as mapped on the child side
- */
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "company")
 public class Company {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Setter(AccessLevel.PRIVATE)
+    @OneToMany(mappedBy = "company")
     private List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        throw new UnsupportedOperationException("I'm still not implemented!");
+        product.setCompany(this);
+        products.add(product);
     }
 
     public void removeProduct(Product product) {
-        throw new UnsupportedOperationException("I'm still not implemented!");
+        product.setCompany(null);
+        products.remove(product);
     }
 }
