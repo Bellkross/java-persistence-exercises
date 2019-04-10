c8e38e47594fe210dd3515c532ffd2f9c9963b8f
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 00:40:16 +0300

    Finish photo-comment-dao task dao part
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
+++ photo-comment-dao/src/main/java/ua/procamp/dao/PhotoDaoImpl.java
@@ -1,9 +1,15 @@
 package ua.procamp.dao;
 
+import org.hibernate.Session;
 import ua.procamp.model.Photo;
+import ua.procamp.model.PhotoComment;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Please note that you should not use auto-commit mode for your implementation.
@@ -17,26 +23,76 @@ public class PhotoDaoImpl implements PhotoDao {
 
     @Override
     public void save(Photo photo) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        consumeWithTx(em -> em.persist(photo));
     }
 
     @Override
     public Photo findById(long id) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        return readWithTx(em ->
+                em.createQuery("select p from photo p where p.id = :id", Photo.class)
+                        .setParameter("id", id).getSingleResult()
+        );
     }
 
     @Override
     public List<Photo> findAll() {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        return readWithTx(em -> em.createQuery("select p from photo p", Photo.class).getResultList());
     }
 
     @Override
     public void remove(Photo photo) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        Objects.requireNonNull(photo);
+        Objects.requireNonNull(photo.getId());
+        consumeWithTx(em -> {
+            Photo ph = em.find(Photo.class, photo.getId());
+            em.remove(ph);
+        });
     }
 
     @Override
     public void addComment(long photoId, String comment) {
-        throw new UnsupportedOperationException("Just do it!"); // todo
+        Objects.requireNonNull(comment);
+        consumeWithTx(em -> {
+            Photo p = em.find(Photo.class, photoId);
+            PhotoComment pc = new PhotoComment();
+            pc.setText(comment);
+            pc.setPhoto(p);
+            em.persist(pc);
+            p.addComment(pc);
+        });
+    }
+
+    private void consumeWithTx(Consumer<EntityManager> entityManagerTFunction) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManagerTFunction.accept(entityManager);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException("Read transaction was failed", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private <T> T readWithTx(Function<EntityManager, T> entityManagerTFunction) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        makeReadOnly(entityManager);
+        try {
+            T result = entityManagerTFunction.apply(entityManager);
+            entityManager.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException("Read transaction was failed", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private void makeReadOnly(EntityManager em) {
+        em.unwrap(Session.class).setDefaultReadOnly(true);
     }
 }
