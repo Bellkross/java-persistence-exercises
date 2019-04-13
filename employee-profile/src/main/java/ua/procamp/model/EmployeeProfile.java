91f89371e5987622fc5d1786426444a6a0d9d36b
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-13 20:22:52 +0300

    Finish employee-profile task
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- employee-profile/src/main/java/ua/procamp/model/EmployeeProfile.java
+++ employee-profile/src/main/java/ua/procamp/model/EmployeeProfile.java
@@ -1,8 +1,11 @@
 package ua.procamp.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 /**
  * todo:
@@ -17,12 +20,22 @@ import lombok.Setter;
  * - map relation between {@link Employee} and {@link EmployeeProfile} using foreign_key column: "employee_id"
  * - configure a derived identifier. E.g. map "employee_id" column should be also a primary key (id) for this entity
  */
-@NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "employee_profile")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class EmployeeProfile {
+    @Id
+    @EqualsAndHashCode.Include
     private Long id;
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "employee_id")
     private Employee employee;
+    @Column(nullable = false)
     private String position;
+    @Column(nullable = false)
     private String department;
 }
